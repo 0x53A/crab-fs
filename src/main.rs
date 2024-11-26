ffeeae28f43db3230a3498ce8a9ede9cd5cde923
Lukas Rieger <code@lukasrieger.com>
2024-11-26 20:11:29 +0100

    wip

--- src/main.rs
+++ src/main.rs
@@ -804,81 +804,14 @@ impl Filesystem for SimpleFS {
         _rdev: u32,
         reply: ReplyEntry,
     ) {
-        let file_type = mode & libc::S_IFMT as u32;
-
-        if file_type != libc::S_IFREG as u32
-            && file_type != libc::S_IFLNK as u32
-            && file_type != libc::S_IFDIR as u32
-        {
-            // TODO
-            warn!("mknod() implementation is incomplete. Only supports regular files, symlinks, and directories. Got {:o}", mode);
-            reply.error(libc::ENOSYS);
-            return;
-        }
-
-        if self.lookup_name(parent, name).is_ok() {
-            reply.error(libc::EEXIST);
-            return;
-        }
-
-        let mut parent_attrs = match self.get_inode(parent) {
-            Ok(attrs) => attrs,
-            Err(error_code) => {
-                reply.error(error_code);
-                return;
+        match self.mknod_syn(req, parent, name, mode, _umask, _rdev) {
+            Ok(ok) => {
+                reply.entry(&ok.ttl, &ok.attrs, ok.generation);
+            },
+            Err(err_code) => {
+                reply.error(err_code);
             }
-        };
-
-        if !check_access(
-            parent_attrs.uid,
-            parent_attrs.gid,
-            parent_attrs.mode,
-            req.uid(),
-            req.gid(),
-            libc::W_OK,
-        ) {
-            reply.error(libc::EACCES);
-            return;
         }
-        parent_attrs.last_modified = time_now();
-        parent_attrs.last_metadata_changed = time_now();
-        self.write_inode(&parent_attrs);
-
-        if req.uid() != 0 {
-            mode &= !(libc::S_ISUID | libc::S_ISGID) as u32;
-        }
-
-        let inode = self.allocate_next_inode();
-        let attrs = InodeAttributes {
-            inode,
-            // open_file_handles: 0,
-            size: 0,
-            last_accessed: time_now(),
-            last_modified: time_now(),
-            last_metadata_changed: time_now(),
-            kind: as_file_kind(mode),
-            mode: self.creation_mode(mode),
-            hardlinks: 1,
-            uid: req.uid(),
-            gid: creation_gid(&parent_attrs, req.gid()),
-            xattrs: Default::default(),
-        };
-        self.write_inode(&attrs);
-        File::create(self.content_path(inode)).unwrap();
-
-        if as_file_kind(mode) == FileKind::Directory {
-            let mut entries = BTreeMap::new();
-            entries.insert(b".".to_vec(), (inode, FileKind::Directory));
-            entries.insert(b"..".to_vec(), (parent, FileKind::Directory));
-            self.write_directory_content(inode, entries);
-        }
-
-        let mut entries = self.get_directory_content(parent).unwrap();
-        entries.insert(name.as_bytes().to_vec(), (inode, attrs.kind));
-        self.write_directory_content(parent, entries);
-
-        // TODO: implement flags
-        reply.entry(&Duration::new(0, 0), &attrs.into(), 0);
     }
 
     fn mkdir(
@@ -1660,6 +1593,93 @@ type ReplyDataResult = Result<ReplyDataOk, c_int>;
 
 
 impl SimpleFS {
+
+    fn mknod_syn(
+        &mut self,
+        req: &Request,
+        parent: u64, 
+        name: &OsStr,
+        mut mode: u32,
+        _umask: u32,
+        _rdev: u32,
+     ) -> ReplyEntryResult {
+        let file_type = mode & libc::S_IFMT as u32;
+
+        if file_type != libc::S_IFREG as u32
+            && file_type != libc::S_IFLNK as u32
+            && file_type != libc::S_IFDIR as u32
+        {
+            // TODO
+            warn!("mknod() implementation is incomplete. Only supports regular files, symlinks, and directories. Got {:o}", mode);
+            reply.error(libc::ENOSYS);
+            return;
+        }
+
+        if self.lookup_name(parent, name).is_ok() {
+            reply.error(libc::EEXIST);
+            return;
+        }
+
+        let mut parent_attrs = match self.get_inode(parent) {
+            Ok(attrs) => attrs,
+            Err(error_code) => {
+                reply.error(error_code);
+                return;
+            }
+        };
+
+        if !check_access(
+            parent_attrs.uid,
+            parent_attrs.gid,
+            parent_attrs.mode,
+            req.uid(),
+            req.gid(),
+            libc::W_OK,
+        ) {
+            reply.error(libc::EACCES);
+            return;
+        }
+        parent_attrs.last_modified = time_now();
+        parent_attrs.last_metadata_changed = time_now();
+        self.write_inode(&parent_attrs);
+
+        if req.uid() != 0 {
+            mode &= !(libc::S_ISUID | libc::S_ISGID) as u32;
+        }
+
+        let inode = self.allocate_next_inode();
+        let attrs = InodeAttributes {
+            inode,
+            // open_file_handles: 0,
+            size: 0,
+            last_accessed: time_now(),
+            last_modified: time_now(),
+            last_metadata_changed: time_now(),
+            kind: as_file_kind(mode),
+            mode: self.creation_mode(mode),
+            hardlinks: 1,
+            uid: req.uid(),
+            gid: creation_gid(&parent_attrs, req.gid()),
+            xattrs: Default::default(),
+        };
+        self.write_inode(&attrs);
+        File::create(self.content_path(inode)).unwrap();
+
+        if as_file_kind(mode) == FileKind::Directory {
+            let mut entries = BTreeMap::new();
+            entries.insert(b".".to_vec(), (inode, FileKind::Directory));
+            entries.insert(b"..".to_vec(), (parent, FileKind::Directory));
+            self.write_directory_content(inode, entries);
+        }
+
+        let mut entries = self.get_directory_content(parent).unwrap();
+        entries.insert(name.as_bytes().to_vec(), (inode, attrs.kind));
+        self.write_directory_content(parent, entries);
+
+        // TODO: implement flags
+        reply.entry(&Duration::new(0, 0), &attrs.into(), 0);
+     }
+
     fn symlink_syn(
         &mut self,
         req: &Request,
